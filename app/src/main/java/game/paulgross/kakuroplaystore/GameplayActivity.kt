14b72cedc955a8a26229edcf15d920c25bd2f6d7
Paul Gross <pauligross@gmail.com>
2023-07-31 18:34:42 +1000

    Initial commit

---
+++ app/src/main/java/game/paulgross/kakuroplaystore/GameplayActivity.kt
@@ -0,0 +1,11 @@
+package game.paulgross.kakuroplaystore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplay)
+    }
+}
