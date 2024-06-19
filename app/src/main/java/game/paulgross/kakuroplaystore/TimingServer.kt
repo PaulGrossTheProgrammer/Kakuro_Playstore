b0fcd6a0b7821630ca7ff39cdeb0cc7422941261
Paul Gross <pauligross@gmail.com>
2024-06-19 21:06:29 +1000

    Adding timing server for display animation.

--- app/src/main/java/game/paulgross/kakuroplaystore/TimingServer.kt
+++ app/src/main/java/game/paulgross/kakuroplaystore/TimingServer.kt
@@ -2,6 +2,8 @@ package game.paulgross.kakuroplaystore
 
 class TimingServer: Thread() {
 
+    private var running = true
+
     // TODO - change the second String to the new Type that stores the timing data.
     private val timerLookup = mutableMapOf<String, EventTimer>()
 
@@ -9,9 +11,22 @@ class TimingServer: Thread() {
 
     override fun run() {
         // TODO - run until shutdown, sleeping between known events.
+        while (running) {
 
-        // Adding a new timer to timerLookup will wake the thread and force a recalc
-        // of the sleep time.
+
+            // Adding a new timer to timerLookup will wake the thread and force a recalc
+            // of the sleep time.
+
+            // Iterate all the timer tasks and send messages as required.
+            // For each timer task, determine the sleep time required.
+            // Keep track of only the shortest sleep time, and at the end, sleep for that time.
+        }
+
+        /// Shutdown everything here ... clear maps and lists etc.
+    }
+
+    public fun shutdownTimerSystem() {
+        running = false
     }
 
     public fun addSingleEvent(eventName: String) {
@@ -21,4 +36,10 @@ class TimingServer: Thread() {
     public fun addPeriodicEvent(eventName: String) {
         // TODO
     }
+
+    public fun cancelEvent(eventId: Int) {
+
+    }
+
+    // Also allow events to tbe queried by id.
 }
