5581a84cf74e406a963123246cda3a3aee1aae4d
Paul Gross <pauligross@gmail.com>
2024-06-20 01:20:00 +1000

    Adding timing server for display animation.

--- app/src/main/java/game/paulgross/kakuroplaystore/TimingServer.kt
+++ app/src/main/java/game/paulgross/kakuroplaystore/TimingServer.kt
@@ -2,25 +2,31 @@ package game.paulgross.kakuroplaystore
 
 import game.paulgross.kakuroplaystore.GameEngine.Message
 
-class TimingServer: Thread() {
+class TimingServer() : Thread() {
 
-    // TODO - move this to inside the game engine, and all it to be requested.
+    var serverThread: TimingServer? = null
+
+    // TODO - move this to inside the game engine, and allow it to be requested with a factory function.
     // The game engine will start it and shut it down as required.
     // The game engine will pause and unpause it as required too. Need to research and test that feature.
 
     private var running = true
 
-    private val timerLookup = mutableMapOf<String, EventTimer>()
+    private val eventTimers = mutableListOf<EventTimer>()
 
-    data class EventTimer(var id: Int, val responseFunction: (message: Message) -> Unit, val start: Long, val duration: Int, val periodic: Boolean)
+    data class EventTimer(val responseFunction: (message: Message) -> Unit, val delay: Int, val periodic: Boolean, val syncTime: Long)
 
     override fun run() {
+        serverThread = this
+        println("#### STARTING TIMER SYSTEM.")
+
+
         var sleepTime = 10000L // Default 10 seconds
-                // TODO - run until shutdown, sleeping between known events.
+
+        // TODO - run until shutdown, sleeping between sending event messages.
         while (running) {
 
-
-            // Adding a new timer to timerLookup will wake the thread and force a recalc
+            // Adding a new timer to eventTimers will wake the thread and force a recalc
             // of the sleep time.
 
             // Iterate all the timer tasks
@@ -30,35 +36,54 @@ class TimingServer: Thread() {
 
             // For each timer task, determine the sleep time required.
             // Keep track of only the shortest sleep time, and at the end, sleep for that time.
+            for (et in eventTimers) {
+                println("#### TODO - calc sleep delay for [$et].")
+            }
 
-            println("TimingServer sleeping for $sleepTime milliseconds")
-            sleep(sleepTime)
+            // Check running flag again in case it was switched off during message handling.
+            if (running) {
+                println("#### TimingServer sleeping for $sleepTime milliseconds.")
+                try {
+                    sleep(sleepTime)
+                } catch (e: InterruptedException) {
+                    println("#### TimingServer was INTERRUPTED while sleeping.")
+                }            }
         }
 
-        /// Shutdown everything here ... clear maps and lists etc.
+        // Shutdown everything here ... clear maps and lists etc.
+        // ?? Iterate each EventTimer and send a shutdown message to the callbacks???
+
+        eventTimers.clear()
     }
 
     public fun shutdownTimerSystem() {
         running = false
+        serverThread?.interrupt()
     }
 
-    public fun addSingleEvent(eventName: String) {
-        // TODO
+    public fun addSingleEvent(responseFunction: (message: Message) -> Unit, delay: Int): EventTimer {
+        val et = EventTimer(responseFunction, delay, false, System.currentTimeMillis())
+        eventTimers.add(et)
+        serverThread?.interrupt()    // Force a immediate assessment of the timing
+        return et
     }
 
-    public fun addPeriodicEvent(eventName: String) {
-        // TODO
+    public fun addPeriodicEvent(responseFunction: (message: Message) -> Unit, period: Int): EventTimer {
+        val et = EventTimer(responseFunction, period, true, System.currentTimeMillis())
+        eventTimers.add(et)
+        serverThread?.interrupt()    // Force a immediate assessment of the timing
+        return et
     }
 
-    public fun setSyncOffset(delta: Int, otherEventId: Int) {
+    public fun setSyncOffset(delta: Int, otherEvent: EventTimer) {
         // Adjust the sync time so that events don't happen too closely.
 
         // The new sync time will be relative to the sync time of the otherEventId.
     }
 
-    public fun cancelEvent(eventId: Int) {
-
+    public fun cancelEvent(eventTimer: EventTimer) {
+        // TODO: Delete event from timerLookup
     }
 
-    // Also allow events to tbe queried by id.
+    // Also allow events to tbe queried by id, for expected delays, last run etc...
 }
